d8ed9f0af8db1808a9f0819742b7ac6e4c4bd4f9
dtischler <18220601+dtischler@users.noreply.github.com>
2021-11-05 14:48:56 -0700

    Update start.sh

--- donkeycar/start.sh
+++ donkeycar/start.sh
@@ -3,4 +3,4 @@ export DISPLAY=:0
 export DBUS_SYSTEM_BUS_ADDRESS=unix:path=/host/run/dbus/system_bus_socket
 X &
 xfce4-session &
-cd /usr/src/app/mysim && python3 manage.py drive
+cd /usr/src/app/mycar && python3 manage.py drive
